f5b4b816bfcbb376c619ec51aabff34c4a063f84
thz <lixuepeng@bilibili.com>
2022-05-04 15:55:20 +0800

    [规范化]

--- renderer.js
+++ renderer.js
@@ -4,12 +4,3 @@
 // `nodeIntegration` is turned off. Use `preload.js` to
 // selectively enable features needed in the rendering
 // process.
-"use strict";
-let canvas = document.getElementById("GLCanvas");
-let gl = canvas.getContext("webgl2") ||
-    canvas.getContext("experimental-webgl2");
-if (gl !== null) {
-    gl.clearColor(0.0, 0.8, 0.0, 1.0);
-    gl.clear(gl.COLOR_BUFFER_BIT);
-}
-document.write("<br><b>The above is WebGL draw area!</b>");
